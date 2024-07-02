e41235258c0e3bb80e0f9601b5638b56c243477c
Anurag Samarth <asamarth017@gmail.com>
2024-07-02 23:39:06 +0530

    Text component is added

--- Day 7 Guess the number/src/App.jsx
+++ Day 7 Guess the number/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header"
+import Text from "./components/Text"
 
 function App() {
   return (
     <>
     <Header />
+    <Text/>
     </>
   )
 }
--- Day 7 Guess the number/src/components/Text.jsx
+++ Day 7 Guess the number/src/components/Text.jsx
@@ -1,7 +1,11 @@
-function Text(){
-    return (<>
-    
-    </>)
+function Text() {
+  return (
+    <>
+      <div className="w-full text-center">
+        <p>Enter a guess between 0 to 100</p>
+      </div>
+    </>
+  );
 }
 
 export default Text;
