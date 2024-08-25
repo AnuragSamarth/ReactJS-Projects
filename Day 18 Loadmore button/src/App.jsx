2419ca866e40751c0beb2b8bdad81b2f33fed505
Anurag Samarth <asamarth107@gmail.com>
2024-08-25 23:07:54 +0530

    loading more button

---
+++ Day 18 Loadmore button/src/App.jsx
@@ -0,0 +1,14 @@
+// import './App.css'
+import LoadMoreData from './Components/LoadMoreData/LoadMoreData'
+
+function App() {
+
+
+  return (
+    <>
+     <LoadMoreData />
+    </>
+  )
+}
+
+export default App
